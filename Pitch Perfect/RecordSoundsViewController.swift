c5d8d4c71cbb79817ba7f6186a23abf128559ca2
Yilen <yilen.pan@gmail.com>
2015-03-12 21:32:53 -0700

    removed mp3

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -21,15 +21,12 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     override func viewWillAppear(animated: Bool) {
-        //Hide the stop button
         stopButton.hidden = true
         recordButton.enabled = true
         tapToRecord.hidden = false
